abfdfe57066a1e6f4740887727950c60d3dab77a
Matthew Taylor <majikpig@gmail.com>
2020-07-19 19:36:44 -0400

    initial setup and questions

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,29 @@
-/* 
+/*
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
+const jwt = require("jsonwebtoken")
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const authError = {
+    message: "Not authorized",
+  }
+
+  try {
+    const token = req.cookies.token
+    if (!token) {
+      return res.status(401).json(authError)
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json(authError)
+      }
+
+      next()
+    })
+  } catch(err) {
+    next(err)
+  }
 };
