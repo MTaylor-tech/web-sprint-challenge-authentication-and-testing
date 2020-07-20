a7a0b7b7bab37805bb67bae361f392147b44698b
Matthew Taylor <majikpig@gmail.com>
2020-07-19 21:31:49 -0400

    fixed authentication test

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -12,15 +12,34 @@ module.exports = (req, res, next) => {
 
   try {
 
-    if (!req.headers || !req.headers.authorization) {
+    let token
+
+    if ((!req.headers || !req.headers.authorization) && !req.cookies) {
       return res.status(401).json(authError)
     }
 
-    const token = req.headers.authorization.split(";")[0].split("=")[1]
+    if (req.cookies && req.cookies.token) {
+      token = req.cookies.token
+    } else {
+
+      if (req.headers.authorization.includes(";")) {
+        token = req.headers.authorization.split(";")[0].split("=")[1]
+      } else {
+        token = req.headers.authorization.split(" ")[1]
+      }
+      console.log(`Authorization: ${req.headers.authorization}`)
+    }
+    
+    console.log(`Token: ${token}`)
+
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      console.log(decoded)
       if (err) {
         return res.status(401).json(authError)
+      } else {
+        req.userId = decoded.userId
+        req.username = decoded.username
       }
 
       next()
